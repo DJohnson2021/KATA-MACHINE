68835f66ca916ca910ec5f362a05164236090ab9
Devin Johnson <j00847797@students.jsums.edu>
2024-02-09 01:39:32 -0600

    reviewing doubly linked lists

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,44 @@
+type Node<T> = {
+    data: T,
+    next?: Node<T>,
+}
+
+
 export default class SinglyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     prepend(item: T): void {
+        const node = {data: item} as Node<T>;
 
-}
+        this.length++;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head = node;
+    }
     insertAt(item: T, idx: number): void {
 
-}
+    }
     append(item: T): void {
 
-}
+    }
     remove(item: T): T | undefined {
 
-}
+    }
     get(idx: number): T | undefined {
 
-}
+    }
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
