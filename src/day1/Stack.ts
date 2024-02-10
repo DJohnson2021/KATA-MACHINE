3869a96700e0f3ec9d7db832a5b2388fe3ab55a1
Devin Johnson <j00847797@students.jsums.edu>
2024-02-09 21:31:58 -0600

    Refreshed my knowledge of linked list based stacks

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
-
+    private top?: Node<T>;
     
 
     constructor() {
+        this.length = 0;
+        this.top = undefined;
     }
 
     push(item: T): void {
+        const node = {value: item} as Node<T>;
+        this.length++;
 
-}
+
+        if (!this.top) {
+            this.top = node;
+        }
+
+        node.next = this.top;
+        this.top = node;
+    }
     pop(): T | undefined {
+        if (this.length === 0) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const out = this.top;
+        this.top = this.top?.next;
+        return out?.value;
+
+    }
     peek(): T | undefined {
-
-}
+        return this.top?.value;
+    }
 }
